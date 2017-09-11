5dd92cb86cce4081e3e4bf59ba77c861d99c5bca
Lauren Schaefer <lschaefer@sugarcrm.com>
2017-09-11 15:11:05 -0400

    Dynamically set the blog author

--- WordpressToJive/Script.py
+++ WordpressToJive/Script.py
@@ -7,7 +7,8 @@ from datetime import datetime
 
 namespaces = {
     'content': 'http://purl.org/rss/1.0/modules/content/',
-    'dc': 'http://purl.org/dc/elements/1.1/'
+    'dc': 'http://purl.org/dc/elements/1.1/',
+    'wp': 'http://wordpress.org/export/1.2/'
     }
 
 access_token = ""
@@ -29,12 +30,71 @@ def authenticateToJive():
         raise ValueError('Unable to get access token from response. ' + str(r.status_code) + ' ' + r.content)
     print 'Successfully authenticated and parsed access token.'
     
+def getJiveUsername(firstName, lastName):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token
+        }
+    r = requests.get(config.jiveUrl + 'api/core/v3/search/people?filter=search(' + firstName + ',' + lastName + ')', headers=headers)
+    if r.status_code == 200:       
+        return json.loads(r.content).get('list')[0].get('jive').get('username')
+    else:
+        raise RuntimeError('An error occurred while getting the the username for ' + firstName + ' ' + lastName + '. ' + str(r.status_code) + ' ' + r.content)
+
+#use the authorId from the <dc:creator> tag to get the creator's first and last names from the Wordpress file     
+def getAuthorNames(authorId):
+    authorNames = {
+        "authorId": authorId
+    }
+    
+    authors = xml.etree.ElementTree.parse(config.wordpressFileToParse).getroot().find('channel').findall('wp:author', namespaces)
+    for author in authors:
+        if author.find('wp:author_login', namespaces).text == authorId:
+            authorNames["firstName"] = author.find('wp:author_first_name', namespaces).text
+            authorNames["lastName"] = author.find('wp:author_last_name', namespaces).text
+            return authorNames
+    return authorNames
+
+# Check to see if we can find an author id in Jive for the Wordpress author.
+# If not, we'll set the author to the currently logged in user    
+def getAuthor(authorId):
+    authorNames = getAuthorNames(authorId);
+    try:
+        if authorNames.get('firstName') is not None and authorNames.get('lastName') is not None:
+            authorNames['jiveUsername'] = getJiveUsername(authorNames.get('firstName'), authorNames.get('lastName'))
+            return authorNames
+    except Exception as e:
+        print ('An error occurred while trying to get the Jive username associated with ' + authorId + '. The authorId will be set to ' + config.username)
+        print e
+    authorNames['jiveUsername'] = config.username
+    return authorNames   
+
+def processBlogContent(content, authorNames):
+    if content is None:
+        return content
+    
+    if authorNames.get('jiveUsername') is config.username:
+        firstName = authorNames.get('firstName')
+        lastName = authorNames.get('lastName')
+        
+        nameString = None
+        if firstName is not None:
+            nameString = firstName
+        if lastName is not None:
+            nameString = nameString + " " + lastName
+        if nameString is None and authorNames.get('authorId') is not None:
+            nameString = authorNames.get('authorId')
+        if nameString is not None:
+            content = "<p>Post originally written by " + nameString + ".</p>" + content
+    
+    return content 
+    
 def createBlogPost(title, author, pubDate, content):
     print 'Creating blog post'
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + access_token,
-        'X-Jive-Run-As': 'username ' + author #TODO: handle when usernames don't match
+        'X-Jive-Run-As': 'username ' + author
         }
     params = {
         "published": pubDate.strftime('%Y-%m-%dT%H:%M:%S +%f'),
@@ -57,15 +117,27 @@ def createBlogPost(title, author, pubDate, content):
         raise RuntimeError('An error occurred while creating blog post: ' + title + '. ' + str(r.status_code) + ' ' + r.content)
     
 def processWordpressFile():
+    authenticateToJive()
+    
     root = xml.etree.ElementTree.parse(config.wordpressFileToParse).getroot()
     channel = root.find('channel')
-    for item in channel.findall('item'):
-        title = item.find('title').text
-        author = item.find('dc:creator', namespaces).text
-        content = item.find('content:encoded', namespaces).text
-        pubDate = datetime.strptime(item.find('pubDate').text, '%a, %d %b %Y %H:%M:%S +%f')
-        createBlogPost(title, author, pubDate, content)    
+    
+    for item in channel.findall('item'):   
+        try:
+            title = item.find('title').text
+            
+            authorNames = getAuthor(item.find('dc:creator', namespaces).text)
+            
+            content = processBlogContent(item.find('content:encoded', namespaces).text, authorNames)
+            
+            pubDate = datetime.strptime(item.find('pubDate').text, '%a, %d %b %Y %H:%M:%S +%f')
+            createBlogPost(title, authorNames.get('jiveUsername'), pubDate, content) 
+        except Exception as e:
+            print
+            print ('Warning!  A blog post was not successfully created.')
+            print e
+           
 
-authenticateToJive()
+
 processWordpressFile()
 
