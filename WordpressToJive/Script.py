1a8c55117f4176b51971805e5a2c2835de3347cb
Lauren Schaefer <lschaefer@sugarcrm.com>
2017-09-11 11:14:45 -0400

    Create blog post
    Added config file
    Successfully creates a blog post with some of the basic info (needs a
    lot of fine tuning)

--- WordpressToJive/Script.py
+++ WordpressToJive/Script.py
@@ -1,25 +1,64 @@
 import xml.etree.ElementTree
-
-root = xml.etree.ElementTree.parse('/Users/lschaefer/Downloads/sugardeveloper.wordpress.com-2017-09-11-12_04_39/sugardeveloperblog-sugarcrm.wordpress.2017-09-11.post_type-post.start_date-2017-09-01.end_date-2017-09-30.001.xml').getroot()
-# print(root)
-# print(root.tag)
-# 
-# for node in root:
-#     print node
-#     print node.keys()
-#     print node.items()
-#     for subnode in node:
-#         print subnode
-#         print subnode.keys()
-#         print subnode.items()
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import config
 
 namespaces = {
     'content': 'http://purl.org/rss/1.0/modules/content/',
     'dc': 'http://purl.org/dc/elements/1.1/'
     }
 
-channel = root.find('channel')
-for item in channel.findall('item'):
-    print item.find('title').text
-    print item.find('dc:creator', namespaces).text
-    print item.find('content:encoded', namespaces).text
+access_token = ""
+
+# Authenticate to Jive and get the access token the other requests will use   
+def authenticateToJive():
+    print 'Authenticating to Jive'
+    payload = (
+        ('grant_type', 'password'),
+        ('username', config.username),
+        ('password', config.password)
+        )
+    r = requests.post(config.jiveUrl + 'oauth2/token', auth=HTTPBasicAuth(config.client_id, config.client_secret), data=payload)
+    if r.status_code != 200:
+        raise RuntimeError('Unable to authenticate to Jive. ' + str(r.status_code) + ' ' + r.content)
+    global access_token;
+    access_token = json.loads(r.content).get('access_token')
+    if not access_token:
+        raise ValueError('Unable to get access token from response. ' + str(r.status_code) + ' ' + r.content)
+    print 'Successfully authenticated and parsed access token.'
+    
+def createBlogPost(title, author, content):
+    print 'Creating blog post'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token,
+        }
+    requestBody = {
+        "content": {
+            "type": "text/html",
+            "text": content
+        },
+        "subject": title,
+        "type": "post",
+        "parent": config.jiveUrl + "api/core/v3/places/" + config.jivePlaceId
+        
+        }
+    r = requests.post(config.jiveUrl + 'api/core/v3/contents', headers=headers, json=requestBody)
+    if r.status_code == 201:
+        print 'Successfully created blog post: ' + title
+    else:
+        raise RuntimeError('An error occurred while creating blog post: ' + title + '. ' + str(r.status_code) + ' ' + r.content)
+    
+def processWordpressFile():
+    root = xml.etree.ElementTree.parse(config.wordpressFileToParse).getroot()
+    channel = root.find('channel')
+    for item in channel.findall('item'):
+        title = item.find('title').text
+        author = item.find('dc:creator', namespaces).text
+        content = item.find('content:encoded', namespaces).text
+        createBlogPost(title, author, content)    
+
+authenticateToJive()
+processWordpressFile()
+
---
+++ WordpressToJive/config.py
@@ -0,0 +1,10 @@
+client_id = ''
+client_secret = ''
+
+username = ''
+password = '!'
+
+jiveUrl = ''
+jivePlaceId = ''
+
+wordpressFileToParse = ''
